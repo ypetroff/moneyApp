9276f50da231d2c27980044ba179b531bb20c3f5
Yordan Petrov <yordan.petroff@gmail.com>
2023-01-19 22:19:05 +0200

    Initial commit

---
+++ src/main/java/bg/softuni/moneyapp/MoneyAppApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.moneyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/moneyapp/MoneyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.moneyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
