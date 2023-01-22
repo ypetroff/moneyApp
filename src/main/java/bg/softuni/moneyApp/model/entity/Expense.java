c94b3fc1e99841de8a3529f848838ec339018c7e
Yordan Petrov <yordan.petroff@gmail.com>
2023-01-22 17:25:52 +0200

    Add annotations for Expense entity

--- src/main/java/bg/softuni/moneyApp/model/entity/Expense.java
+++ src/main/java/bg/softuni/moneyApp/model/entity/Expense.java
@@ -5,7 +5,7 @@ import bg.softuni.moneyApp.model.enums.ExpenseCategory;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "expenses")
@@ -15,15 +15,22 @@ public class Expense extends BaseEntity{
     @Column(name = "expense_category", nullable = false)
     private ExpenseCategory category;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    //TODO: When the user inputs totalPrice, pricePerUnit and numberOfUnits could be null
+    @Column(name = "price_per_unit")
     private BigDecimal pricePerUnit;
 
+    @Column(name = "number_of_units")
     private Integer numberOfUnits;
 
+    @Column(name = "total_price", nullable = false)
     private BigDecimal totalPrice;
 
-    private LocalDate dateOfPurchase;
+    //NOTE: another option - set a default param for expenseDTO OR set default in the controller
+    @Column(name = "date_of_purchase", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime dateOfPurchase;
 
     @Enumerated(value = EnumType.STRING)
     @Column(name = "currency", columnDefinition = "BGN") //TODO: check possible submit of null value
