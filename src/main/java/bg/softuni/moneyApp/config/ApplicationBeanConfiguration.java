7e56eafe75671acb8cfc8665080ce4b4d2464126
Yordan Petrov <yordan.petroff@gmail.com>
2023-01-28 22:23:28 +0200

    Add modelmapper and gson to ApplicationBeanConfiguration

--- src/main/java/bg/softuni/moneyApp/config/ApplicationBeanConfiguration.java
+++ src/main/java/bg/softuni/moneyApp/config/ApplicationBeanConfiguration.java
@@ -1,9 +1,38 @@
 package bg.softuni.moneyApp.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationBeanConfiguration {
 
+    @Bean
+    public ModelMapper modelMapper() {
+
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.addConverter(mappingContext ->
+                        LocalDateTime.parse(mappingContext.getSource(),
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                String.class,
+                LocalDateTime.class);
+
+        return modelMapper;
+    }
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+//                .serializeNulls() //NOTE: serialize null values if necessary
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
+    }
 
 }
