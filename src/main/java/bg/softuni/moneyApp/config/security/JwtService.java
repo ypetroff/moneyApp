78c2954be4e87cd567358597bb0089075f70ee7d
Yordan Petrov <yordan.petroff@gmail.com>
2023-03-10 23:12:33 +0200

    Implementation of extractUsername method

--- src/main/java/bg/softuni/moneyApp/config/security/JwtService.java
+++ src/main/java/bg/softuni/moneyApp/config/security/JwtService.java
@@ -1,10 +1,39 @@
 package bg.softuni.moneyApp.config.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY =
+            "66546A576D5A7134743777217A25432A462D4A614E645267556B587032723575";
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
