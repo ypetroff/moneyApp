bcfe925d2c52af1fdaa7faa71b0cad0ce6cb9ef9
Yordan Petrov <yordan.petroff@gmail.com>
2023-03-14 20:19:05 +0200

    Implementation of methods within the JwtService

--- src/main/java/bg/softuni/moneyApp/config/security/JwtAuthenticationFilter.java
+++ src/main/java/bg/softuni/moneyApp/config/security/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -34,5 +35,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+        }
     }
 }
